8af3986c92629c3cec2c56e57d135fed64b98a0f
unknown <w2cluster589@iiht.tech>
2020-11-10 16:39:06 +0530

    third commit

--- convertamountservices/src/main/java/com/ibm/convertamountservice/ConvertamountservicesApplication.java
+++ convertamountservices/src/main/java/com/ibm/convertamountservice/ConvertamountservicesApplication.java
@@ -3,8 +3,12 @@ package com.ibm.convertamountservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableHystrix
+@EnableHystrixDashboard
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
--- convertamountservices/src/main/java/com/ibm/convertamountservice/service/AmountConversionService.java
+++ convertamountservices/src/main/java/com/ibm/convertamountservice/service/AmountConversionService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import com.ibm.convertamountservice.dto.AmountDTO;
 import com.ibm.convertamountservice.restclient.CurrencyClient;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @Service
 public class AmountConversionService {
@@ -14,9 +15,18 @@ final CurrencyClient client;
 		this.client=client;
 	}
 	
+	@HystrixCommand(fallbackMethod ="currencyServiceFallback")
 	public AmountDTO getConvertedAmount(Long id,AmountDTO dto) {
 		Double conversionFactor = client.getConversionFactor(id, dto.getCountryCode());
 		Double convertedAmount = dto.getAmount()*conversionFactor;
+		dto.setAmount(convertedAmount);
+		dto.setId(id);
+		return dto;
+	}
+	
+	public AmountDTO currencyServiceFallback(Long id, AmountDTO dto) {
+		Double conversionfactor = 1.0;
+		Double convertedAmount = dto.getAmount()*conversionfactor;
 		dto.setAmount(convertedAmount);
 		dto.setId(id);
 		return dto;
