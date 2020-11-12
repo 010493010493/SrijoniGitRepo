5850336df6d8885e85a7a8bd195eee8e0aa2923c
unknown <w2cluster589@iiht.tech>
2020-11-12 18:00:53 +0530

    seventh commit

--- convertamountservices/src/main/java/com/ibm/convertamountservice/restclient/CurrencyClient.java
+++ convertamountservices/src/main/java/com/ibm/convertamountservice/restclient/CurrencyClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name="currency-service")
+@FeignClient(name = "currency-service"/* ,url = "http://192.168.99.100:8500" */)
 public interface CurrencyClient {
 	
 	@GetMapping("/currency/{id}")
